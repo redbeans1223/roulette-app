2430c2f78107d76471a0f1e9d5fd7448c12f6d91
redbeans0821 <redbeans_iorm6@ezweb.ne.jp>
2025-09-20 14:08:00 +0900

    Fix Chache

--- frontend/src/components/RouletteWheel.js
+++ frontend/src/components/RouletteWheel.js
@@ -4,70 +4,79 @@ const RouletteWheel = ({sections, rotation, colors, isSpinning}) => {
     const canvasRef = useRef(null);
     const animationFrameRef = useRef(null);
     const startTimeRef = useRef(null);
+    const boardChacheRef = useRef(null); // ルーレット台のキャッシュ用
 
     useEffect(() => {
         const canvas = canvasRef.current;
         
-        const ctx = canvas.getContext('2d');
         const { count, type, labels } = sections;
-
+        
         const width = 330; const height = 330;
         const centerX = width / 2; const centerY = height / 2; const radius = width / 2 - 30;
+        const offCanvas = document.createElement('canvas');
+        offCanvas.width = width; offCanvas.height = height;
+        const ctx = offCanvas.getContext('2d');
         
-        canvas.width = width; canvas.height = height;
         // ルーレット描画
         const angle = (2 * Math.PI) / count; // count=8 なら angle = 0.785398163（ラジアン） で 一つの円弧の円周でもある
+        ctx.beginPath();
+        ctx.arc(centerX, centerY, radius + 30, 0, 2 * Math.PI); // 外枠
+        ctx.fillStyle = 'darkgreen';
+        ctx.fill();
+        ctx.closePath();
+        ctx.strokeStyle = 'darkgreen';
+        ctx.stroke();
+        // セクション描画
+        for (let i = 0; i < count; i++) {
+            const startAngle = i * angle;
+            const endAngle = (i + 1) * angle;
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+            ctx.lineWidth = 0.5;
+            ctx.lineTo(centerX, centerY);
+            ctx.fillStyle = colors[i % colors.length] || '#000000';
+            ctx.fill();
+            ctx.strokeStyle = 'black';
+            ctx.stroke();
+            
+            // ラベル描画
+            ctx.save();
+            ctx.translate(centerX, centerY);
+            ctx.rotate(startAngle + angle / 2);
+            ctx.translate(type === 'number' ? radius * 0.8 : radius * 0.7, 0);
+            ctx.rotate(Math.PI / 2);
+            ctx.fillStyle = 'white';
+            ctx.font = type == 'number' ? '28px Arial' : '14px Arial';
+            ctx.textAlign = "center";
+            ctx.textBaseline = 'middle';
+            const text = type == 'number' ? String(i + 1) : labels[i] || `セクション${i + 1}`;
+            ctx.fillText(text, 0, 0);
+            ctx.restore();  
+        }
+        boardChacheRef.current = offCanvas; // ルーレット台のキャッシュ
+    }, [sections, colors]);
+    // 毎フレーム描画
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        const width = canvas.width; const height = canvas.height;
+        const centerX = width / 2; const centerY = height / 2; const radius = width / 2 - 30;
+        
         const draw = (currentRotation, indicatorAngle) => {
             ctx.clearRect(0, 0, width,  height);
-            // ルーレット台の描画
-            ctx.beginPath();
-            ctx.arc(centerX, centerY, radius + 30, 0, 2 * Math.PI); // 外枠
-            ctx.fillStyle = 'darkgreen';
-            ctx.fill();
-            ctx.closePath();
-            ctx.strokeStyle = 'darkgreen';
-            // ctx.lineWidth = 5;
-            ctx.stroke();
-            ctx.save();
-            // ルーレット本体の描画
-            ctx.translate(centerX, centerY);
-            ctx.rotate(currentRotation - 0.1 || 0);
-            ctx.translate(-centerX, -centerY);
-            
-            for (let i = 0; i < count; i++) {
-                const startAngle = i * angle;
-                const endAngle = (i + 1) * angle;
-                // セクション描画
-                ctx.save();
-                ctx.beginPath();
-                ctx.arc(centerX, centerY, radius, startAngle, endAngle);
-                ctx.lineWidth = 0.5;
-                ctx.lineTo(centerX, centerY);
-                ctx.fillStyle = colors[i % colors.length] || '#000000';
-                ctx.fill();
-                ctx.closePath();
-                ctx.strokeStyle = 'black';
-                ctx.stroke();
-                ctx.restore();
+            if (boardChacheRef.current) {
                 ctx.save();
                 ctx.translate(centerX, centerY);
-                ctx.rotate(startAngle + angle / 2);
-                ctx.translate(type === 'number' ? radius * 0.8 : radius * 0.7, 0);
-                ctx.rotate(Math.PI / 2);
-                ctx.fillStyle = 'white';
-                ctx.font = type == 'number' ? '28px Arial' : '14px Arial';
-                ctx.textAlign = "center";
-                ctx.textBaseline = 'middle';
-                const text = type == 'number' ? String(i + 1) : labels[i] || `セクション${i + 1}`;
-                ctx.fillText(text, 0, 0);
-                ctx.restore();  
+                ctx.rotate(currentRotation - 0.1 || 0);
+                ctx.translate(-centerX, -centerY);
+                ctx.drawImage(boardChacheRef.current, 0, 0);
+                ctx.restore();
             }
-            ctx.restore();
             // インジケータの描画
             ctx.save();
             
             ctx.translate(centerX, centerY - radius -5);
-            ctx.rotate(indicatorAngle); // 揺れ角度
+            ctx.rotate(indicatorAngle);
             ctx.beginPath();
             ctx.moveTo(-8, -8);
             ctx.lineTo(8, -8);
@@ -78,7 +87,14 @@ const RouletteWheel = ({sections, rotation, colors, isSpinning}) => {
             ctx.strokeStyle = 'gray';
             ctx.stroke();
             ctx.restore();
+
+            // 軸の描画
+            ctx.beginPath();
+            ctx.arc(centerX, centerY - radius - 20, 8, 0, 2 * Math.PI);
+            ctx.fillStyle = 'black';
+            ctx.fill();
         };
+          
         if (isSpinning) {
             const animate = (timestamp) => {
                 if (!startTimeRef.current) startTimeRef.current = timestamp;
